50967de06350efaed70e7f05f29963e648c1b9d6
Wiktor Susfal <wiktor.susfal@gmail.com>
2024-08-26 21:22:22 +0200

    Fixed filepaths display, adjusted loading dialog

--- tests/gui/vw_loading.py
+++ tests/gui/vw_loading.py
@@ -4,6 +4,5 @@ from tests.gui._base import visual_test_preview
 
 if __name__ == '__main__':
     view = VwLoading()
-    view.show()
-    view._load_gif.start()
+    view.manage(True)
     visual_test_preview(view)
